bafb4cde7833c084ee6dd2d4e257adc146f62e43
omega <nithiajo7@gmail.com>
2023-02-03 18:54:55 +0800

    fixed key problem

--- src/services/createURL.js
+++ src/services/createURL.js
@@ -2,10 +2,9 @@ const ASTRONOMY_PIC_OF_THE_DAY = "astronomypicoftheday";
 const APOD_URL = '/planetary/apod';
 const MARS_URL = '/mars-photos/api/v1/rovers/';
 
-const key = `api_key=${process.env.REACT_APP_NASA_API_KEY}`;
 
 const createURL = (id, date) => {
-    const url = (id === ASTRONOMY_PIC_OF_THE_DAY) ?  `${APOD_URL}?date=${date}&${key}`  : `${MARS_URL}${id}/photos?earth_date=${date}&page=1&${key}`;
+    const url = (id === ASTRONOMY_PIC_OF_THE_DAY) ?  `${APOD_URL}?date=${date}&${process.env.REACT_APP_NASA_API_KEY}`  : `${MARS_URL}${id}/photos?earth_date=${date}&page=1&${process.env.REACT_APP_NASA_API_KEY}`;
 
     return url;
 }
